886ec06993229e550aacabe834c84260c5451858
DeathStorm <Death_Storm@web.de>
2015-12-16 20:50:25 +0100

    LevelGen

---
+++ Assets/Editor/Editor_LevelGenRandom.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(LevelGenRandom))]
+public class Editor_LevelGenRandom : Editor {
+
+    public override void OnInspectorGUI()
+    {
+       
+
+
+        if (GUILayout.Button("Regenerate Map"))
+        {
+            LevelGenRandom levelGenRandom = (LevelGenRandom)target;
+            levelGenRandom.GenerateInitialMap();
+        }
+
+        base.OnInspectorGUI();
+    }
+}
---
+++ Assets/Ressources/Scripts/LevelGenRandom.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+public class LevelGenRandom : MonoBehaviour {
+
+
+    public List<GameObject> wallPrefabs;
+    public int startLevelDepth = 6;
+    public float depthOffSet = 0.32f;
+    public float gameWidth = 6.0f;
+
+    private GameObject levelHolder;
+
+	// Use this for initialization
+	void Start () 
+    {
+ 
+	
+	}
+
+    public void GenerateInitialMap()
+    {
+        if (levelHolder == null)
+        {
+            levelHolder = new GameObject();
+            levelHolder.name = "Level Holder";
+            //levelHolder.AddComponent<Transform>();
+            levelHolder.transform.position = new Vector3(0, 0, 0);
+
+        }
+        else
+        {
+            foreach (Transform childtransform in levelHolder.transform)
+            {
+                if (Application.isEditor)
+                { DestroyImmediate(childtransform.gameObject); }
+                else
+                { Destroy(childtransform.gameObject); }
+            }
+        }
+
+        for (int depth = 0; depth <= startLevelDepth; depth++)
+        {
+            GameObject wallPrefab;
+
+            wallPrefab = wallPrefabs[Random.Range(0, wallPrefabs.Count)];
+            GameObject wallObjectRight = (GameObject)GameObject.Instantiate(wallPrefab, new Vector3(gameWidth / 2, depth * depthOffSet, 0), wallPrefab.transform.rotation);
+            wallObjectRight.transform.SetParent(levelHolder.transform);
+
+            wallPrefab = wallPrefabs[Random.Range(0, wallPrefabs.Count)];
+            GameObject wallObjectLeft = (GameObject)GameObject.Instantiate(wallPrefab, new Vector3((gameWidth / 2)*-1, depth * depthOffSet, 0), wallPrefab.transform.rotation);
+            wallObjectLeft.transform.Rotate(new Vector3(0,0,180));
+            wallObjectLeft.transform.SetParent(wallObjectRight.transform);
+
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	
+	}
+}
